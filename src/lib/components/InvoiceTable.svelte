3e637ab1029555e4afcc9cbee95200485c529e99
VC Technosolutions <vctechops1@gmail.com>
2025-03-28 01:19:40 +0530

    Update InvoiceTable.svelte

--- src/lib/components/InvoiceTable.svelte
+++ src/lib/components/InvoiceTable.svelte
@@ -337,15 +337,14 @@
         class="absolute right-0 mt-2 w-36 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 z-10"
       >
         <div class="py-1">
-          {['CSV', 'EXCEL', 'PDF'].map(format => (
-            <button 
-              key={format}
-              on:click={() => handleDownload(format)}
-              class="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-            >
-              {format}
-            </button>
-          ))}
+        {#each ['CSV', 'EXCEL', 'PDF'] as format (format)}
+          <button 
+            on:click={() => handleDownload(format)}
+            class="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+          >
+            {format}
+          </button>
+        {/each}
         </div>
       </div>
     {/if}
