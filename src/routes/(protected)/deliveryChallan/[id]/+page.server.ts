48f090511c966a8b9f3b0946b07670f69c39c91b
VC Technosolutions <vctechops1@gmail.com>
2025-04-01 02:35:06 +0530

    Create +page.server.ts

---
+++ src/routes/(protected)/deliveryChallan/[id]/+page.server.ts
@@ -0,0 +1,37 @@
+import type { PageServerLoad } from './$types';
+import { redirect } from '@sveltejs/kit';
+
+async function getToken(fetch: typeof globalThis.fetch): Promise<string> {
+    const tokenResponse = await fetch('/api/zohoAuthToken');
+    const { token } = await tokenResponse.json();
+    return token;
+}
+
+export const load: PageServerLoad = async ({ params, fetch, locals }) => {
+
+    
+    if (!locals.user) {
+        throw redirect(302, new URL('/login', 'https://vc-tech.vercel.app/').toString());
+    }
+
+    const token = await getToken(fetch);
+    const deliveryChallanId = params.id;
+
+    const response = await fetch(`https://www.zohoapis.in/books/v3/deliverychallans/${deliveryChallanId}?organization_id=60005679410`, {
+        headers: {
+            'Authorization': `Zoho-oauthtoken ${token}`
+        }
+    });
+
+    if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    const deliveryChallan = data.deliverychallan;
+    console.log("data: ", deliveryChallan);
+
+    return {
+        deliveryChallan
+    };
+};
