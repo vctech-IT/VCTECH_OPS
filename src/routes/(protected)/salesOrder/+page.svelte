339ac0d4dcbfb4a11f090bf428080ff26060cbab
VC Technosolutions <vctechops1@gmail.com>
2025-04-01 02:33:36 +0530

    Update +page.svelte

--- src/routes/(protected)/salesOrder/+page.svelte
+++ src/routes/(protected)/salesOrder/+page.svelte
@@ -788,11 +788,11 @@
 
 <style>
   .loader {
-    border: 5px solid #f3f3f3;
-    border-top: 5px solid #3498db;
+    border: 4px solid #f3f3f3;
+    border-top: 4px solid #3498db;
     border-radius: 50%;
-    width: 50px;
-    height: 50px;
+    width: 40px;
+    height: 40px;
     animation: spin 1s linear infinite;
   }
 
