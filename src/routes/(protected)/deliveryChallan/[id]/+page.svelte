a323f6b5727c2bd3557f64fd3b1dfae49dfa91bc
VC Technosolutions <vctechops1@gmail.com>
2025-04-03 17:39:55 +0530

    Update +page.svelte

--- src/routes/(protected)/deliveryChallan/[id]/+page.svelte
+++ src/routes/(protected)/deliveryChallan/[id]/+page.svelte
@@ -2,7 +2,7 @@
 <script lang="ts">
   import { onMount } from 'svelte';
   import { goto } from '$app/navigation';
-  import { ArrowLeft, Download, ChevronDown } from 'svelte-lucide';
+  import { ArrowLeft, Download, ChevronDown, PaperClip } from 'svelte-lucide';
   import '$lib/styles/app.css';
   import LoadingOverlay from '$lib/components/LoadingOverlay.svelte';
   import CustomLoader from '$lib/components/CustomLoader.svelte';
