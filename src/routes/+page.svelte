9c8159437c7d290b223231fa968af6900f1dea48
VC Technosolutions <vctechops1@gmail.com>
2025-04-05 11:56:17 +0530

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -815,13 +815,12 @@ onDestroy(() => {
       <div class="flex items-center space-x-4 mb-4">
 
       <button 
-      class="bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded-md text-sm flex items-center"
+      class="bg-sky-950 hover:bg-blue-700 text-white px-4 py-2 rounded-md text-sm flex items-center"
       on:click={() => fetchDashboardData()}
     >
       <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15" />
       </svg>
-      Refresh
     </button>
         <!-- Order Status Filter -->
         <div class="relative">
